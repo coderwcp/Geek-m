c0c3501ca10ae9120f99b35ea68a6ba5554942a1
WCP <1129342154@qq.com>
2022-05-22 16:07:12 +0800

    配置 Redux 的相关类型

---
+++ src/types/store.d.ts
@@ -0,0 +1,16 @@
+// 用来存放跟 Redux 相关类型，比如，action 的类型等
+
+import store from "@/store";
+import { ThunkAction } from "redux-thunk";
+// Redux 中状态的类型
+export type RootState = ReturnType<typeof store.getState>;
+
+// 使用 thunk 中间件后的 Redux dispatch 类型
+// ReturnType：thunk action 的返回类型
+// State: Redux 的状态  RootState
+// ExtraThunkArg: 额外的参数，没有用到，可以指定为 unknown
+// BasicAction: 非 thunk action，即对象形式的 action
+export type RootThunkAction = ThunkAction<void, RootState, unknown, RootAction>;
+
+// 项目中所有 action 的类型
+type RootAction = unknown;
