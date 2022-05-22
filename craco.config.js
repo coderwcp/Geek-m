d69e183a8b1bf58c1f6cf5539221c2b78b5546a2
WCP <1129342154@qq.com>
2022-05-22 15:45:07 +0800

    移动端适配

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,10 @@
-const path = require('path');
+const path = require("path");
+// // 将 px 转化为 vw 工具包
+// const pxToViewport = require("postcss-px-to-viewport");
+// const vw = pxToViewport({
+//   // 视口宽度，一般就是 375（ 设计稿一般采用二倍稿，宽度为 375 ）
+//   viewportWidth: 375,
+// });
 
 module.exports = {
   // webpack 配置
@@ -6,10 +12,16 @@ module.exports = {
     // 配置别名
     alias: {
       // 约定：使用 @ 表示 src 文件所在路径
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
       // 约定：使用 @scss 表示全局 SASS 样式所在路径
       // 在 SASS 中使用
-      '@scss': path.resolve(__dirname, 'src/assets/styles'),
+      "@scss": path.resolve(__dirname, "src/assets/styles"),
     },
   },
+
+  // style: {
+  //   postcss: {
+  //     plugins: [vw],
+  //   },
+  // },
 };
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,10 @@
 import { Button } from "antd-mobile";
+import "./index.scss";
 
 function Login() {
   return (
     <div>
+      <div className="box"></div>
       <Button color="primary"> 登录</Button>
     </div>
   );
