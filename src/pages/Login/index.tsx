a980a5d6844c178238a5301993321f4256c5073f
WCP <1129342154@qq.com>
2022-05-22 15:46:43 +0800

    使用字体图标

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,6 +5,10 @@ function Login() {
   return (
     <div>
       <div className="box"></div>
+      <svg className="icon" aria-hidden="true">
+        {/* 使用时，只需要将此处的 iconbtn_like_sel 替换为 icon 的名称即可*/}
+        <use xlinkHref="#iconbtn_like_sel"></use>
+      </svg>
       <Button color="primary"> 登录</Button>
     </div>
   );
